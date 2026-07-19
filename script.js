2c5be263e97c6e85b8d76876c8270cd4c6c77175
Alex <89216539+alexco07@users.noreply.github.com>
2026-07-18 23:00:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // =========================================================
 // CONFIGURATION
 // =========================================================
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://vakuntnisjddjbzpiwvn.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZha3VudG5pc2pkZGpienBpd3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0MjIyNjcsImV4cCI6MjA5OTk5ODI2N30.rsWyO0wiGRQn-gOqtW4LaH3IwcvLhpe1wXT6Yd5c49U';
 const MODEL_URL = "https://alexco07.github.io/Timesheet-2/models";
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
